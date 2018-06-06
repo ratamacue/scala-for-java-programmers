b37249d3831c03410b2691643965f255ccaaaf89
David Ron <david.m.ron@gmail.com>
2018-06-06 03:12:38 +0000

    Scala upgrade, case classes and pattern matching.

--- src/test/scala/com/davidron/scalaforjava/primitives.scala
+++ src/test/scala/com/davidron/scalaforjava/primitives.scala
@@ -16,7 +16,7 @@ class NumbersAreObjectsTest{
         assertEquals( looksLikeJava , theyAreActuallyObjects )
 }
 
-class FunctionsAreObjectTest{
+class FunctionsAreObjectsTest{
     
     def thisFunctionReturnsAFunction() : String=>String = {
         string=>"This is a string: "+string
@@ -25,5 +25,31 @@ class FunctionsAreObjectTest{
     @Test
     def testNumbersAreObject() =  
         assertEquals( thisFunctionReturnsAFunction( )( "Ho!" ) , "This is a string: Ho!" )
+}
+
+class TraitsAndPatternMatchingTest {
+    abstract class Notification
+
+    case class Email(sender: String, title: String, body: String) extends Notification
+
+    case class SMS(caller: String, message: String) extends Notification
+
+    case class VoiceRecording(contactName: String, link: String) extends Notification
+
+    def getNotification(notification: Notification): String = {
+      notification match {
+        case Email(email, title, _) => 
+          s"You got an email from $email with title: $title"
+        case SMS(number, message) =>
+          s"You got an SMS from $number! Message: $message"
+        case VoiceRecording(name, link) =>
+          s"you received a Voice Recording from $name! Click the link to hear it: $link"
+      }
+    }
+    
+    @Test
+    def testPatternMatching() =  
+        assertEquals( getNotification(SMS(caller="David", message="Hello")) , "You got an SMS from David! Message: Hello" )
+    
 }
 
